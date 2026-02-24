383a4cb3b5a435d7670bdd2045ca0840c38e6ea7
PixelStarGames88 <PixelStarGames020320@mail.ru>
2026-02-24 11:41:10 +0300

    Small settings of project.

---
+++ Boss of the GYM/Controllers/AccountController.cs
@@ -0,0 +1,15 @@
+﻿using Boss_of_the_GYM.Pages;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Boss_of_the_GYM.Controllers;
+public class AccountController : Controller
+{
+    public IActionResult Register() => View();
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Register(RegisterModel registerViewModel)
+    {
+        return RedirectToAction("Index", "Home");
+    }
+}
--- Boss of the GYM/Program.cs
+++ Boss of the GYM/Program.cs
@@ -1,6 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+builder.Services.AddMvc();  
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapControllerRoute(name: "default", pattern: "{controller=Account}/{action=Register}/{id?}");
 
 app.Run();
